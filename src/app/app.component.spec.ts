dc98da50d22f39404cf45f7c0131eb7d3f7a007f
Gabriel Ataide <gabriel.ataide07@gmail.com>
2024-03-02 14:59:18 -0300

    feat: unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,4 +19,29 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('2023 Sales Report');
   });
+
+  it(`should have some data to show on screen`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.data?.length).toBeGreaterThan(0)
+  });
+
+  it(`should have at least 3 categories`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.data?.length).toBeGreaterThanOrEqual(3)
+  });
+
+  it(`should have at least 3 products for each category`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    let test = true;
+    for (let i = 0; i < app.data?.length; i++) {
+      if (app.data[i]?.products?.length < 3) {
+        test = false;
+        break;
+      }
+    }
+    expect(test).toBeTrue()
+  });
 });
