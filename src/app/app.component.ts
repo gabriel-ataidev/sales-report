1270734d7f74b43b337848d500605d9ea31f73b8
Gabriel Ataide <gabriel.ataide07@gmail.com>
2024-03-02 14:52:39 -0300

    feat: selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,51 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { CommonModule } from '@angular/common'
 import { data } from './data'
-import { Category } from './types'
+import { Category, Brand, Product, Profit } from './types'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = '2023 Sales Report'
   data: Category[] = data
+  filteredProducts: Product[] = this.data[0].products
+  filteredBrands: Brand[] = this.data[0].products[0].brands
+  filteredProfits: Profit[] = data[0].products[0].brands[0].profit
+  initialYLabel: number[] = this.filteredProfits.map(item => item.sales)
+  initialXLabel: string[] = this.filteredProfits.map(item => item.month)
+
+  onInputChange (target: any): void {
+    let item
+    switch (target.name) {
+      case 'category':
+        item = this.data.find(item => item.category === target.value)
+        if (item) {
+          this.filteredProducts = item.products
+          this.filteredBrands = this.filteredProducts[0].brands
+          this.filteredProfits = this.filteredBrands[0].profit
+        }
+        break
+      case 'product':
+        item = this.filteredProducts.find(item => item.product === target.value)
+        if (item) {
+          this.filteredBrands = item.brands
+          this.filteredProfits = this.filteredBrands[0].profit
+        }
+        break
+      case 'brand':
+        item = this.filteredBrands.find(item => item.brand === target.value)
+        if (item) {
+          this.filteredProfits = item.profit
+        }
+        break
+      default:
+        break
+    }
+  }
 }
